a605c70c9cd8e53e9e54dc7647ab6d3de72a5bce
cewing <cris@crisewing.com>
2014-03-15 13:57:54 -0700

    fixing messes

--- src/plone/login/browser/login.py
+++ src/plone/login/browser/login.py
@@ -1,13 +1,9 @@
-from plone.login.interfaces import ILogin
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.Five.browser import BrowserView
-
-from z3c.form import form
-from z3c.form import field
 
 
 class LoginView(BrowserView):
     pass
+
 
 class RegisterView(BrowserView):
     pass
