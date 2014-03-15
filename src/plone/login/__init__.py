42d457375e7bcf8d997a3ed3d1a580ae1737d95b
cewing <cris@crisewing.com>
2014-03-15 12:15:02 -0700

    add message factories for translations

--- src/plone/login/__init__.py
+++ src/plone/login/__init__.py
@@ -1,3 +1,5 @@
 # -*- extra stuff goes here -*-
-
+import zope.i18nmessageid
+MessageFactory = zope.i18nmessageid.MessageFactory('plone.login')
+PloneMessageFactory = zope.i18nmessageid.MessageFactory('plone')
 
