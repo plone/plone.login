955260d1ba385d052eaaa279d4b9c459484a468c
cewing <cris@crisewing.com>
2014-03-15 12:36:01 -0700

    add login form and views

--- src/plone/login/browser/login.py
+++ src/plone/login/browser/login.py
@@ -1,5 +1,27 @@
+from plone.login.interfaces import ILogin
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.Five.browser import BrowserView
+
+from z3c.form import form
+from z3c.form import field
 
 
 class InsufficientPrivilegesView(BrowserView):
     pass
+
+
+class LoginForm(form.Form):
+    field = field.Fields(ILogin)
+    ignoreContext = True
+    label = "Content status history dates"
+
+
+class LoginView(BrowserView):
+    template = ViewPageTemplateFile('templates/login.pt')
+
+    def __init__(self, context, request):
+        super(LoginView, self).__init__(context, request)
+
+        self.login_form = LoginForm(context, request)
+        self.login_form.updateWidgets()
+
