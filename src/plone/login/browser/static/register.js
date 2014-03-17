ca8f1c857c3a9faacdbfda952bd327bcb7933461
Chris Calloway <cbc@chriscalloway.org>
2014-03-16 19:53:14 -0700

    Attempt at adding attributes and classes to z3c.form fields in register template via javascript.

---
+++ src/plone/login/browser/static/register.js
@@ -0,0 +1,8 @@
+// Functions used by registration form
+
+$(document).ready(function() {
+    $("#username").attr("placeholder", "Username");
+    $("#username").attr("autocapitalize", "off");
+    $("#username").attr("tabindex", "1");
+    $("#username").addClass("stretch");
+});
