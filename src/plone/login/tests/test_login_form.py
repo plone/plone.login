7a67e49a43525c239866453040b74d9ce59c02b7
Franco Pellegrini <frapell@gmail.com>
2014-06-20 15:06:55 -0500

    This test was pointless and not really testing anything

--- src/plone/login/tests/test_login_form.py
+++ src/plone/login/tests/test_login_form.py
@@ -7,9 +7,6 @@ from Products.CMFCore.utils import getToolByName
 from plone.app.z3cform.interfaces import IPloneFormLayer
 from plone.login.interfaces import IPloneLoginLayer
 
-from plone.app.testing import TEST_USER_NAME
-from plone.app.testing import TEST_USER_PASSWORD
-#from plone import api
 from plone.login.testing import \
     PLONE_LOGIN_INTEGRATION_TESTING
 
@@ -53,16 +50,3 @@ class TestLoginForm(unittest.TestCase):
 
         data, errors = form.form_instance.extractData()
         self.assertEqual(len(errors), 0)
-
-    def test_user_logged_in(self):
-        self._setup_authenticator_request()
-        self.request['__ac_name'] = TEST_USER_NAME
-        self.request['__ac_password'] = TEST_USER_PASSWORD
-        self.request['form.widgets.came_from'] = [u'']
-        form = self.portal.restrictedTraverse(FORM_ID)
-        form.update()
-        form.form_instance.handleLogin(form.form_instance, 'http://nohost')
-
-        #data, errors = form.form_instance.extractData()
-        member = self.mt.getAuthenticatedMember()
-        self.assertEqual(member.getUserName(), 'test-user')
