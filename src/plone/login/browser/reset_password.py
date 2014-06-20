44c26b480014e6f3d54d83f6c069a7e69d4dddf7
Andy Leeb <ableeb@andersonleeb.com>
2014-06-20 15:10:00 -0500

    pep8

--- src/plone/login/browser/reset_password.py
+++ src/plone/login/browser/reset_password.py
@@ -4,6 +4,7 @@ from Products.CMFCore.utils import getToolByName
 from AccessControl import Unauthorized
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.statusmessages.interfaces import IStatusMessage
+from plone import api
 from plone.login import MessageFactory as _
 from plone.login.interfaces import IResetPasswordForm
 from plone.z3cform import layout
@@ -15,7 +16,7 @@ from z3c.form import form
 
 class ResetPasswordForm(form.EditForm):
     """ Implementation of the reset password form """
-        
+
     fields = field.Fields(IResetPasswordForm)
 
     id = "ResetPasswordForm"
@@ -33,22 +34,23 @@ class ResetPasswordForm(form.EditForm):
         super(ResetPasswordForm, self).updateWidgets(prefix="")
 
         self.widgets['password'].tabindex = 1
-        klass = getattr(self.widgets['password'],'klass','')
+        klass = getattr(self.widgets['password'], 'klass', '')
         if klass:
             self.widgets['password'].klass = ' '.join([klass, _(u'stretch')])
         else:
             self.widgets['password'].klass = _(u'stretch')
         self.widgets['password'].placeholder = _(u'Super secure password')
         self.widgets['password_confirm'].tabindex = 2
-        klass = getattr(self.widgets['password_confirm'],'klass','')
+        klass = getattr(self.widgets['password_confirm'], 'klass', '')
         if klass:
-            self.widgets['password_confirm'].klass = ' '.join([klass, _(u'stretch')])
+            self.widgets['password_confirm'].klass = ' '.join(
+                [klass, _(u'stretch')])
         else:
             self.widgets['password_confirm'].klass = _(u'stretch')
         self.widgets['password_confirm'].placeholder = _(u'Confirm password')
 
     @button.buttonAndHandler(_('Reset'), name='reset')
-    def handleRegister(self, action):
+    def handlePasswordReset(self, action):
 
         authenticator = getMultiAdapter((self.context, self.request),
                                         name=u"authenticator")
@@ -59,6 +61,17 @@ class ResetPasswordForm(form.EditForm):
         if errors:
             self.status = self.formErrorsMessage
             return
+
+        password = str(data.get('password'))
+        password2 = str(data.get('password_confirm'))
+
+        current = api.user.get_current()
+        pw_tool.resetPassword(current, randomstring, password)
+
+        IStatusMessage(self.request).addStatusMessage(
+            _(u"Your password has been reset."), "info")
+
+        self.request.response.redirect(self.context.absolute_url())
 
 
 class ResetPasswordFormView(layout.FormWrapper):
